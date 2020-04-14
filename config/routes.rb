2badc57e8670f1ab99e3f3fa1133208ed08c4436
CBreakr <github email address>
2020-04-14 12:25:23 -0400

    added header with simple style, made games index page (and set to root)

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :properties
   resources :players
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "games#index"
 end
